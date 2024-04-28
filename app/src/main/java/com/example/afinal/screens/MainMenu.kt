54de79536488a94858900078d7d0dcee145a5d20
Alexander Applegate <43449402+CommodoreApples@users.noreply.github.com>
2024-04-27 23:36:45 -0400

    Updated the UI, now have a Main Menu

--- app/src/main/java/com/example/afinal/screens/MainMenu.kt
+++ app/src/main/java/com/example/afinal/screens/MainMenu.kt
@@ -1,12 +1,17 @@
 package com.example.afinal.screens
 
-import androidx.compose.material.Button
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material3.Text as Text3
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -17,7 +22,13 @@ fun MainMenu(
     onTimerClick: () -> Unit,
     onSettingsClick: () -> Unit,
     onHelpClick: () -> Unit
-) {
+)
+    { Column (
+        modifier = Modifier.fillMaxWidth().padding(start =16.dp)
+    ){
+    Spacer(modifier = Modifier.height((16.dp)))
+    CenteredBoldText(text = "Productivity App")
+    Spacer(modifier = Modifier.height((16.dp)))
     Button(onClick = {
         onCalculatorClick()
     }) {
@@ -50,5 +61,14 @@ fun MainMenu(
         onHelpClick()
     }) {
         Text("Help")
-    }
+    }}
+}
+@Composable
+fun CenteredBoldText(text: String) {
+    Text3(
+        text = text,
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        fontWeight = FontWeight.Bold
+    )
 }
